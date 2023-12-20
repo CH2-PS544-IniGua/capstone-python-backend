91b8624b1b5954b5396f1b5a788a4d5ebd0fe627
Saul Sayers <saulsayers@gmail.com>
2023-12-20 19:43:21 +0700

    fix port

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from app.api.user_routes import router as user_router
 from app.api.fashion_routes import router as fashion_router
@@ -7,7 +8,10 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(fashion_router)
 
+# Get the port number from the environment variable or default to 8000
+port = int(os.environ.get("PORT", 8000))
+
 # For running with `uvicorn` directly
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
